d1e538a97696913145ac41673b667116942a3e3d
Srividyai8 <153827897+Srividyai8@users.noreply.github.com>
2024-08-08 15:46:49 +0530

    Update practise4.java

--- practise4.java
+++ practise4.java
@@ -1,15 +1,20 @@
 package programs;
 
 public class practise4 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//  Write a Java Program to find the factorial of a given number 
-		int a=5,fact=1;
+	public int fact(int a){
+		int fact=1;
 		for(int i=1;i<=a;i++) {
 			fact=fact*i;
 		}
-		System.out.println(fact);
+		return fact;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//  Write a Java Program to find the factorial of a given number 
+		int a=5,factorial;
+		practise4 obj=new practise4();
+		factorial=obj.fact(a);
+		System.out.println(factorial);
 		int f=12;
 		System.out.println(Math.sqrt(f));
 		String c ="srividya";
